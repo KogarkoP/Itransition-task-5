c45319a66197c499cb199c5a2c419df23335cf74
KogarkoP <kogarkopavel@gmail.com>
2025-10-07 17:43:10 +0300

    Edited mail transporter

--- server/src/utils/mailTransporter.js
+++ server/src/utils/mailTransporter.js
@@ -2,9 +2,9 @@ import nodemailer from "nodemailer";
 
 const mailTransporter = () => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "smtp.sendgrid.net",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
